ea804a2d678b0fa7abf0466c2173f4719a888add
castrofullstack <45467289+castrofullstack@users.noreply.github.com>
2026-05-17 17:06:52 -0400

    Add GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,8 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://mba-blog-site.vercel.app',
+  site: 'https://castrofullstack.github.io',
+  base: '/mba-blog-site',
   integrations: [sitemap()],
   markdown: {
     shikiConfig: {
